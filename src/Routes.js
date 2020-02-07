f66ceed84f957396e316ea502b61a4431a8001cf
Justin Htaung <jhtaung29@gmail.com>
2020-02-06 20:26:59 -0800

    setting up...

--- src/Routes.js
+++ src/Routes.js
@@ -1,14 +1,19 @@
 import React from 'react';
-import { BrowserRouter as Router, Switch, Route, Redirect } from 'react-router-dom';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect
+} from 'react-router-dom';
 import { Dashboard, Layout, NotFound } from './views';
 
 const Routes = () => (
   <Router>
     <Switch>
-      <Route exact path='/'><Layout content={<Dashboard />} /></Route>
-      <Route exact path='/dashboard'><Layout content={<Dashboard />} /></Route>
-      <Route exact path='/'><Layout content={<NotFound />} /></Route>
-      <Redirect to='not-found' />
+      <Route exact path="/"><Layout content={<Dashboard />} /></Route>
+      <Route exact path="/dashboard"><Layout content={<Dashboard />} /></Route>
+      <Route exact path="/"><Layout content={<NotFound />} /></Route>
+      <Redirect to="not-found" />
     </Switch>
   </Router>
 );
